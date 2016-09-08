654ac6d40420f183234336a17275caf6d9e2dc08
Andrew Crisp <crispa@slhs.org>
2016-09-08 15:31:07 -0600

    Remove model modification code from lookupModel

--- lookupModel.R
+++ lookupModel.R
@@ -20,63 +20,8 @@ buildSearchTerm <- function (phrase,n){
   searchWords
 }
 
-stripSparseEntries <- function (frequencyTable, percentageToRetain=1){
-  if (percentageToRetain < 0){
-    return(frequencyTable)
-  }
-  if (percentageToRetain >= 1){
-    return(frequencyTable)
-  }
-  
-  totalCount <- sum(frequencyTable$Freq)
-  countToRetain <- totalCount * (percentageToRetain)
-  countToRemove <- totalCount - countToRetain
-  
-  currentCount <- totalCount
-  i <- 1
-  while(currentCount > countToRetain){
-    frequencyTable  <- frequencyTable[frequencyTable$Freq >i,]  
-    currentCount <- sum(frequencyTable$Freq)
-    i <- i + 1
-  }
-  frequencyTable<-droplevels(frequencyTable)
-  frequencyTable
-
-}
-
-stripUnicode <- function (frequencyTable){
-  temp <- frequencyTable[!grepl("[^a-z\\s]",frequencyTable$Term,perl = TRUE),]
-  #temp <- frequencyTable[!grepl("[^[:alnum::space:]]",frequencyTable$Term),]
-  temp <- droplevels(temp)
-  temp
-}
-
-unigramFrequency <- readRDS(unigramFrequencyFile)
-bigramFrequency <- readRDS(bigramFrequencyFile)
-trigramFrequency <- readRDS(trigramFrequencyFile)
-quadgramFrequency <- readRDS(quadgramFrequencyFile)
-pentagramFrequency <- readRDS(pentagramFrequencyFile)
-
-uniSize <- object.size(unigramFrequency)
-biSize <- object.size(bigramFrequency)
-triSize <- object.size(trigramFrequency)
-quadSize <- object.size(quadgramFrequency)
-pentaSize <- object.size(pentagramFrequency)
-sizes <- c("Uni"=uniSize, "Bi" = biSize, "tri"=triSize, "quad"=quadSize, "penta"=pentaSize) / (1024*1024)
-
-newUnis <- stripSparseEntries(unigramFrequency, .95)
-newBis <- stripSparseEntries(bigramFrequency, .70)
-newTris <- stripSparseEntries(trigramFrequency, .50)
-newQuads <- stripSparseEntries(quadgramFrequency, .15)
-newPentas <- stripSparseEntries(pentagramFrequency, .50)
-
-newuniSize <- object.size(newUnis)
-newbiSize <- object.size(newBis)
-newtriSize <- object.size(newTris)
-newquadSize <- object.size(newQuads)
-newpentaSize <- object.size(newPentas)
-newsizes <- c("Uni"=newuniSize,
-              "Bi" = newbiSize,
-              "tri"=newtriSize,
-              "quad"=newquadSize,
-              "penta"=newpentaSize) / (1024*1024)
+unigramFrequency <- readRDS(unigramModelFile)
+bigramFrequency <- readRDS(bigramModelFile)
+trigramFrequency <- readRDS(trigramModelFile)
+quadgramFrequency <- readRDS(quadgramModelFile)
+pentagramFrequency <- readRDS(pentagramModelFile)
