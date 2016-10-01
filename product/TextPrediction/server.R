9d661013d852d3a73ca21fe0e0f90e8d82b65861
Andrew Crisp <crispa@slhs.org>
2016-10-01 10:58:33 -0600

    Handle erroneous input

--- product/TextPrediction/server.R
+++ product/TextPrediction/server.R
@@ -4,15 +4,19 @@ source("lookupModel.R")
 shinyServer(function(input, output) {
   options(DT.options = list(pageLength = 12, dom='t', order = list(list(3,'desc'))))
   
-  output$searchTerm <- renderText(input$searchTerm)
-  
-  output$searchTermLabel <- renderText(paste("Your search term: ", input$searchTerm))
-  
   output$textPrediction <- DT::renderDataTable({
      if(input$searchTerm != ""){
+       output$searchTermLabel <- renderText(paste("Your search term: ", input$searchTerm))
+       
        resultTable <- lookupTerm(as.character(input$searchTerm))
-       output$mostLikelyLabel <- renderText(paste("Most likely phrase: ", predictMostLikely(resultsTable = resultTable)))
-       resultTable
+       
+       if (nrow(resultTable) == 0){
+         output$mostLikelyLabel <- renderText(paste("No results found for the search term."))
+         NULL
+       } else {
+         output$mostLikelyLabel <- renderText(paste("Most likely phrase: ", predictMostLikely(resultsTable = resultTable)))
+         resultTable
+       }
      }
    })
 })
